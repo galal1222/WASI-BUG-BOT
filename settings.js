6c515f817f26148446cd9a361940a7fa1547d285
galal1222 <galalalex3@gmail.com>
2024-08-13 00:31:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "201095521982"
+global.ownername = "🐛 Alex "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '201095521982'  //creator number
+global.ownername = ' MR WASI ' owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
